f6d0823bf5bc0b08028c1740d54d6173d6825dd9
sekhar761007 <91212876+sekhar761007@users.noreply.github.com>
2021-10-08 21:38:08 +0530

    My first github code ever

--- Op.py
+++ Op.py
@@ -1,1 +1,10 @@
-print ("Hii")
+print ("HELLO WORLD THIS IS MY FIRST PROJECT IN GITHUB")
+'''GUYS I AM NOW SERIOUS IN CODING LOL'''
+//In this code i will tell you all how to subtract 5 from 10
+//lets start
+print("Sir:What is the value when you subtract 5 from 10 ?")
+//then the user took help of coding and did this
+A = 10
+B = 5
+print(A-B)
+//here comes the result
